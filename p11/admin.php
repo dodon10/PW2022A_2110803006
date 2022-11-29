ec71689e1e27c99d74868623bcc3c2c075578977
unknown <rizkyramadhan20019@gmail.com>
2022-11-29 23:39:59 +0700

    saya selesai

---
+++ p11/admin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Halaman Admin</title>
+  <style>
+    body {
+      background-color: whitesmoke;
+    }
+
+    .logout {
+      background-color: #AFEEEE;
+      font-weight: bold;
+      padding: 5px 5px;
+      border: 1px solid black;
+      border-radius: 5px;
+      font-size: 14px;
+      text-align: center;
+      width: 60px;
+      height: 20px;
+      position: relative;
+      left: 90%;
+    }
+    .header {
+      color: #708090;
+      font-weight: bold;
+      font-size: 20px;
+      font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+      text-align: center;
+    }
+    .isi {
+      width: 600px;
+      height: 60px;
+      margin: auto;
+      background-color: white;
+      padding: 10px 10px;
+      font-size: 18px;
+      text-align: center;
+      font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    }
+  </style>
+</head>
+<body>
+  <div class="logout">
+    <a href="login.php">Logout</a>
+  </div>
+
+  <div class="header">
+    <h1>Selamat Datang</h1>
+  </div>
+
+  <div class="isi">
+    <p>Hallo Guys Saya Disini</p>
+  </div>
+
+</body>
+
+</html>
